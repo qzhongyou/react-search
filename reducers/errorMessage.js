297b20ca25570afd23810a08bdd7c40214718cce
阙中友 <837300621@qq.com>
2017-12-20 21:09:47 +0800

    当接口正常时,清除错误消息状态

--- reducers/errorMessage.js
+++ reducers/errorMessage.js
@@ -2,6 +2,8 @@ export const errorMessage = (state = null, action)=> {
     const {error} =action;
     if (error) {
         return error
+    }else if(state) {
+        return null
     }
     return state;
 }
