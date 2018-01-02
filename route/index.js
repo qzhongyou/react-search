b38f278c210218beadc3ee67bd11041ffa4af325
阙中友 <837300621@qq.com>
2018-01-02 17:44:08 +0800

    container files rename

--- route/index.js
+++ route/index.js
@@ -1,17 +1,17 @@
 import {Route} from "react-router-dom";
 import  React from "react";
-import App from "../containers/App";
-import exclusiveApp from "../containers/exclusiveApp";
-import similarApp from "../containers/similarApp";
-import serviceDetailApp from "../containers/serviceDetailApp";
+import App from "../containers/AppContainer";
+import Witkey from "../containers/WitkeyContainer";
+import SimilarServices from "../containers/SimilarServicesContainer";
+import ServiceDetail from "../containers/ServiceDetailContainer";
 import Menu from "../components/Menu/Menu";
 export default () => ( <div>
     <Route path="*" render={({match})=> {
         return (<Menu selectedkeys={match.url}/>)
     }}/>
     <Route exact path="/" component={App}/>
-    <Route exact path="/exclusive" component={exclusiveApp}/>
-    <Route exact path="/similar/:id" component={similarApp}/>
-    <Route exact path="/service/:id" component={serviceDetailApp}/>
+    <Route exact path="/exclusive" component={Witkey}/>
+    <Route exact path="/similar/:id" component={SimilarServices}/>
+    <Route exact path="/service/:id" component={ServiceDetail}/>
 </div>)
 
